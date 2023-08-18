a2196d7c8152aec30964ee0cdc4d2a9fa81d69bb
Luís Paulo Santos <psantos@di.uminho.pt>
2023-08-18 11:49:37 +0100

    separated layers evaluation onto own modules 2

--- Feynman_MCSimulator/simulators/layer.hpp
+++ Feynman_MCSimulator/simulators/layer.hpp
@@ -10,4 +10,16 @@
 
 #include <stdio.h>
 
+#include "circuit.h"
+#include "gates.h"
+#include "complex.h"
+
+void layer_w (TCircuitLayer *layer, int l,
+              unsigned long long current_state, unsigned long long next_state, float &wR, float &wI,
+              bool print_debug=false);
+
+float layer_sample (TCircuitLayer* layer, int l, unsigned long long current_state,
+                    unsigned long long& next_state,
+                    float& wR, float& wI);
+
 #endif /* layer_hpp */

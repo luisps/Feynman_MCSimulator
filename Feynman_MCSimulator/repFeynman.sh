0d59b78067b24eeaf52c5780d9a3654132a1446a
Luis Paulo Santos <lpsantos@cirrus08.a.incd.pt>
2024-03-04 15:28:19 +0000

    Updated SLURM scripts

--- Feynman_MCSimulator/repFeynman.sh
+++ Feynman_MCSimulator/repFeynman.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 jobname="Feynman"
 filename="$jobname""_$1_$2_$3_$4_T$5_S$6"
-RL_label=""
-if  [ $9 ]; then
-	filename+="_L$9"
-	RL_label+=" -l $9"
-fi
+R_label=""
 if  [ $7 ] && [ $8 ]; then
-	RL_label+=" -r $7 $8"
+	R_label+=" -r $7 $8"
 fi
+for run in {1..3}
+do
+	filename+="_L${run}"
+	L_label+=" -l ${run}"
 sbatch <<EOT
 #!/bin/bash
 
@@ -22,7 +22,7 @@ sbatch <<EOT
 #SBATCH --mem=10000
 #SBATCH --ntasks-per-node=$5
 
-./Feynman -f ~/QCircuits_BenchTest/circuits/$1/circuit_$1 -a $2 -i $3 -o $4 -t $5 -s $6 $RL_label
+./Feynman -f ~/QCircuits_BenchTest/circuits/$1/circuit_$1 -a $2 -i $3 -o $4 -t $5 -s $6 $R_label $L_label
 
 if [ -s "$filename".e ]; then
 	mv "$filename".e ~/QCircuits_BenchTest/circuits/$1
@@ -37,3 +37,4 @@ fi
 
 exit 0
 EOT
+done
